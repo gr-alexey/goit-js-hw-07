08c0876037966956ad21c01ee8d520470d18e51a
Alex <gryshchenko.oleksii@gmail.com>
2022-06-22 23:00:27 +0300

    attempt-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryList = createGalleryList(galleryItems);
+galleryContainer.insertAdjacentHTML('afterbegin', galleryList);
+
+galleryContainer.addEventListener('click', clickOnImage)
+
+function createGalleryList(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+            <a a class="gallery__link" href = "${original}" >
+                <img
+                    class="gallery__image"
+                    src='${preview}'
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div >`
+    }).join('');
+};
+
+function clickOnImage(e) {
+    e.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.sourse}" width="800" height="600">`);
+    instance.show();
+};
